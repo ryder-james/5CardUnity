6c88df4dda4e367e3c071da192c5b0cd2bb69d7c
Ragger15 <carsonsena@gmail.com>
2021-03-04 19:53:52 -0700

    Add game

---
+++ Assets/Scrpits/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] public Player[] players;
+    [SerializeField] public Card[] mainHand;
+    [SerializeField] public Card[] otherHands;
+    [SerializeField] public Text pot;
+    [SerializeField] public Text bigBlind;
+    [SerializeField] public Text smallBlind;
+    [SerializeField] public Text minBet;
+    [SerializeField] public Button flipButton;
+    [SerializeField] public Image blindIncrease;
+    [SerializeField] public Button discard;
+    [SerializeField] public Button fold;
+    [SerializeField] public Button callCheck;
+    [SerializeField] public Button raiseBet;
+    [SerializeField] public Text mainMoney;
+    [SerializeField] public Text rightMoney;
+    [SerializeField] public Text leftMoney;
+    [SerializeField] public Text topMoney;
+}
