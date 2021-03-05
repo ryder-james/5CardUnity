b415cb8a282235553865b21a83484650bd601c62
Ragger15 <carsonsena@gmail.com>
2021-03-04 18:13:04 -0700

    Add Card

---
+++ Assets/Scrpits/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+[ExecuteInEditMode]
+public class Card : MonoBehaviour
+{
+    [SerializeField] public int rank = 1;
+
+    [SerializeField] public Suit suit;
+
+    [SerializeField] public Sprite[] Cards;
+
+    [SerializeField] public SpriteRenderer sprite;
+
+    private void Update()
+    {
+        sprite.sprite = Cards[rank + ((int)suit * 13) - 1];
+    }
+}
