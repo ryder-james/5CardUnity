c8c85828604fd8021378580c4b9ceb05492d4a04
Ragger15 <carsonsena@gmail.com>
2021-03-08 19:08:39 -0700

    add flip button

--- Assets/Scrpits/Game.cs
+++ Assets/Scrpits/Game.cs
@@ -22,4 +22,53 @@ public class Game : MonoBehaviour
     [SerializeField] public Text rightMoney;
     [SerializeField] public Text leftMoney;
     [SerializeField] public Text topMoney;
+
+    private int[] storeCards = new int[5];
+    private bool flipped = true;
+
+    public void Flip()
+    {
+        if(flipped)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                storeCards[i] = mainHand[i].rank + ((int)mainHand[i].suit * 13 - 1);
+
+                mainHand[i].suit = Suit.CLUB;
+                mainHand[i].rank = 15;
+            }
+            flipped = false; ;
+        }
+        else
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                mainHand[i].rank = (storeCards[i] + 1) % 13;
+                int suit = (storeCards[i] + 1) - mainHand[i].rank;
+                int counter = 0;
+                while (suit != 0)
+                {
+                    suit /= 13;
+                    counter++;
+                }
+                switch(counter)
+                {
+                    case 0:
+                        mainHand[i].suit = Suit.HEART;
+                        break;
+                    case 1:
+                        mainHand[i].suit = Suit.SPADE;
+                        break;
+                    case 2:
+                        mainHand[i].suit = Suit.DIAMOND;
+                        break;
+                    case 3:
+                        mainHand[i].suit = Suit.CLUB;
+                        break;
+                }
+            }
+            flipped = true;
+        }
+    }
+
 }
